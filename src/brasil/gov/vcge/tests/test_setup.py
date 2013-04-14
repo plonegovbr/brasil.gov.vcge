3e1bc16d00324ab7a19793d7875681c2ca7c0da6
Érico Andrei <erico@simplesconsultoria.com.br>
2013-04-14 12:24:00 -0300

    Corrige os testes para lidar com a dependencia do raptus.autocompletewidget

--- src/brasil/gov/vcge/tests/test_setup.py
+++ src/brasil/gov/vcge/tests/test_setup.py
@@ -41,13 +41,13 @@ class TestInstall(BaseTestCase):
         self.assertTrue(self.qi.isProductInstalled(PROJECTNAME),
                         '%s not installed' % PROJECTNAME)
 
-    def test_at_dependencies_installed(self):
+    def test_dependencies_installed(self):
         for product in ['raptus.autocompletewidget', ]:
             self.assertTrue(self.qi.isProductInstalled(product),
                             '%s not installed' % product)
 
     def test_dx_dependencies_installed(self):
-        for product in ['plone.app.dexterity', 'collective.z3cform.widgets']:
+        for product in ['plone.app.dexterity', ]:
             self.assertTrue(self.qi.isProductInstalled(product),
                             '%s not installed' % product)
 
