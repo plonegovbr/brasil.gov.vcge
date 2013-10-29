30a653b21bd4255bbb59719622e5054be4bf44f1
Érico Andrei <erico@simplesconsultoria.com.br>
2013-10-29 13:57:47 -0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0'
+version = '1.1.dev0'
 description = u'Brasil.gov.br: Vocabulário Controlado do Governo Eletrônico'
 long_description = (
     open('README.rst').read() + '\n' +
