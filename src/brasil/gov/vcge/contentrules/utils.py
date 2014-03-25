e3e15c4a8b27acf39cf2c3c069192d24fb391a52
Érico Andrei <erico@simplesconsultoria.com.br>
2014-03-25 15:21:30 -0300

    Documenta funcoes auxiliares

--- src/brasil/gov/vcge/contentrules/utils.py
+++ src/brasil/gov/vcge/contentrules/utils.py
@@ -3,12 +3,16 @@ from Acquisition import aq_base
 
 
 def vcge_available(obj):
-    ''' '''
+    """ Valida se o objeto tem o atributo de
+        armazenamento do VCGE
+    """
     return hasattr(aq_base(obj), 'skos')
 
 
 def vcge_for_object(obj):
-    ''' '''
+    """ Retorna valores armazenados no atributo
+        VCGE de um objeto
+    """
     skos = []
     if hasattr(aq_base(obj), 'skos'):
         skos = obj.skos
@@ -16,7 +20,9 @@ def vcge_for_object(obj):
 
 
 def set_vcge(obj, skos):
-    ''' '''
+    """ Armazena valores no atributo
+        VCGE de um objeto
+    """
     if hasattr(aq_base(obj), 'skos'):
         obj.skos = skos
     return True
