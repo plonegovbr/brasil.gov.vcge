7f89246836c98f2950d995cad1be962ebb723bb6
hvelarde <hector.velarde@gmail.com>
2017-10-31 17:46:05 -0200

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0.1'
+version = '2.0.2.dev0'
 description = u'.gov.br: VocabulĂĄrio Controlado do Governo EletrĂ´nico'
 long_description = (
     open('README.rst').read() + '\n' +
